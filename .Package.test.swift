f1f3b6f68338715eaa20472a13345a1e8ce06120
Syo Ikeda <suicaicoca@gmail.com>
2017-01-02 21:06:37 +0900

    Update ReactiveSwift to 1.0.0 (#17)

--- .Package.test.swift
+++ .Package.test.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "ReactiveCollections",
     dependencies: [
-        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", versions: Version(1, 0, 0, prereleaseIdentifiers: ["rc", "2"])..<Version(1, .max, .max)),
+        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
         .Package(url: "https://github.com/Quick/Quick", majorVersion: 1),
         .Package(url: "https://github.com/Quick/Nimble", majorVersion: 5, minor: 1),
     ]
