cb063ff3cc2f2de472e6c5ddaee9112ceee523b2
Anders Ha <anders@andersha.com>
2017-04-10 00:06:37 +0800

    Update test dependencies (Nimble and Quick). (#18)

--- Package.swift
+++ Package.swift
@@ -1,8 +1,22 @@
+import Foundation
 import PackageDescription
+
+var isSwiftPackagerManagerTest: Bool {
+    return ProcessInfo.processInfo.environment["SWIFTPM_TEST_ReactiveCollections"] == "YES"
+}
 
 let package = Package(
     name: "ReactiveCollections",
-    dependencies: [
-        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
-    ]
+    dependencies: {
+        var deps: [Package.Dependency] = [
+	    .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
+	]
+        if isSwiftPackagerManagerTest {
+            deps += [
+                .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1, minor: 1),
+                .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6, minor: 1),
+            ]
+        }
+	return deps
+    }()
 )
